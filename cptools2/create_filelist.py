ebabea7b8c12c22156c7f43c44549d90f219fa66
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-01-25 10:47:07 +0000

    option to truncate filelist, makes for easier rsync command

--- cptools2/create_filelist.py
+++ cptools2/create_filelist.py
@@ -2,12 +2,30 @@ import glob
 import os
 import parserix
 
-def files_from_plate(plate_dir, ext=".tif", clean=True):
-    """return all proper image files from a plate directory"""
+def files_from_plate(plate_dir, ext=".tif", clean=True, truncate=True):
+    """
+    return all proper image files from a plate directory
+
+    Parameters:
+    -----------
+    plate_dir : string
+        path to plate directory
+    ext : string (default=".tif")
+        image extension, only used if clean is True
+    clean : Boolean (default=True)
+        whether to remove thumbnails and non-image files
+    truncate : Boolean (default=True)
+        whether to truncate the image path to just plate name onwards
+    """
     files = glob.glob(plate_dir + "/*/*/*" + ext)
     if clean is True:
         files = parserix.clean.clean(files)
-    return [os.path.abspath(f) for f in files]
+    if truncate is True:
+        # get the last 4 directories including final file
+        # sorry
+        return [os.path.join(*i.split(os.sep)[-4:]) for i in files]
+    else:
+        return [os.path.abspath(f) for f in files]
 
 
 def paths_to_plates(experiment_directory):
