e3f25bea9270d196e1ffb6fd922e9426ce128e67
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-01-27 13:37:48 +0000

    add pyyaml as a dependency

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ setup(name="cptools2",
       packages=["cptools2"],
       tests_require=["pytest"],
       dependency_links=["https://github.com/swarchal/parserix/tarball/master#egg=parserix-0.1"],
-      install_requires=["pandas>=0.16", "parserix>=0.1"])
+      install_requires=["pyyaml", "pandas>=0.16", "parserix>=0.1"])
