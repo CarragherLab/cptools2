71dee59dee096465b2c818d304587f5ea1733b31
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-01-30 16:06:59 +0000

    remove unused renaming function

--- cptools2/pre_stage.py
+++ cptools2/pre_stage.py
@@ -48,24 +48,6 @@ def cast_dataframe(dataframe):
     return wide_df
 
 
-def update_file_paths(dataframe, cols, original, replacement):
-    """
-    change file paths (could just use sed)
-
-    Parameters:
-    ------------
-    dataframe : pandas dataframe
-    cols : list
-        columns in which to replace the paths.
-    original : string
-        original text to be replaced
-    replacement : string
-        string with with original is replaced by
-    """
-    df_sub = dataframe[cols]
-    return df_sub.applymap(lambda x: x.replace(original, replacement))
-
-
 def rsync_string(filelist, source, destination):
     """
     Create rsync string pointing to a file-list and a destination
