eb9f32980988a8945985a576b85286f854387725
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-01-30 15:10:43 +0000

    re-order functions to make more sense

--- cptools2/job_splitter.py
+++ cptools2/job_splitter.py
@@ -1,11 +1,16 @@
 from parserix import parse as _parse
 import pandas as _pd
 
-def split(img_list, job_size=96):
-    """split imagelist into an imagelist per job containing job_size images"""
-    df_img = _well_site_table(img_list)
-    grouped_list = _group_images(df_img)
-    return [chunk for chunk in chunks(grouped_list, job_size)]
+
+def _well_site_table(img_list):
+    """return pandas dataframe with metadata columns"""
+    final_files = [_parse.img_filename(i) for i in img_list]
+    df_img = _pd.DataFrame({
+        "img_paths"     : img_list,
+        "Metadata_well" : [_parse.img_well(i) for i in final_files],
+        "Metadata_site" : [_parse.img_site(i) for i in final_files]
+        })
+    return df_img
 
 
 def _group_images(df_img):
@@ -21,18 +26,14 @@ def _group_images(df_img):
     return grouped_list
 
 
-def _well_site_table(img_list):
-    """return pandas dataframe with metadata columns"""
-    final_files = [_parse.img_filename(i) for i in img_list]
-    df_img = _pd.DataFrame({
-        "img_paths"     : img_list,
-        "Metadata_well" : [_parse.img_well(i) for i in final_files],
-        "Metadata_site" : [_parse.img_site(i) for i in final_files]
-        })
-    return df_img
-
-
 def chunks(list_like, job_size):
     """generator to split list_like into job_size chunks"""
     for i in range(0, len(list_like), job_size):
         yield list_like[i:i+job_size]
+
+
+def split(img_list, job_size=96):
+    """split imagelist into an imagelist per job containing job_size images"""
+    df_img = _well_site_table(img_list)
+    grouped_list = _group_images(df_img)
+    return [chunk for chunk in chunks(grouped_list, job_size)]
