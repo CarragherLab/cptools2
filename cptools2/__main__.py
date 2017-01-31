507058770e03c517186ff97f4574b24e34823187
Scott Warchal <S.Warchal@sms.ed.ac.uk>
2017-01-31 15:52:14 +0000

    add_plate parsing to job

--- cptools2/__main__.py
+++ cptools2/__main__.py
@@ -9,6 +9,7 @@ def main():
     experiment_args = parse_yaml.experiment(yaml_dict)
     chunk_args = parse_yaml.chunk(yaml_dict)
     remove_plate_args = parse_yaml.remove_plate(yaml_dict)
+    add_plate_args = parse_yaml.add_plate(yaml_dict)
     create_command_args = parse_yaml.create_commands(yaml_dict)
     # some of the optional arguments might be none, in which case don't
     # pass them as arguments to the methods
@@ -16,6 +17,8 @@ def main():
     jobber.add_experiment(**experiment_args)
     if remove_plate_args is not None:
         jobber.remove_plate(**remove_plate_args)
+    if add_plate_args is not None:
+        jobber.add_plate(**add_plate_args)
     if chunk_args is not None:
         jobber.chunk(**chunk_args)
     jobber.create_commands(**create_command_args)
