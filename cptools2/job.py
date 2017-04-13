86f8d0b2b856785b9500ed549b897ac65dfbce47
Scott Warchal <S.Warchal@sms.ed.ac.uk>
2017-04-13 10:35:09 +0100

    remove utils.order_dict... function that doesnt exist anymore

--- cptools2/job.py
+++ cptools2/job.py
@@ -148,8 +148,6 @@ class Job(object):
             self._create_loaddata()
         cp_commands, rsync_commands, rm_commands = [], [], []
         commands.make_output_directories(location=location)
-        # transform plate_store into an ordered dictionary and sort by plate-name
-        self.plate_store = utils.order_dictionary_by_key(self.plate_store)
         # for each job per plate, create loaddata and commands
         platenames = sorted(self.plate_store.keys())
         for plate in platenames:
