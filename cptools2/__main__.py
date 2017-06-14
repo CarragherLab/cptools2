bc95310997c804d55cda7db2536cb13d751377ef
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-06-14 10:50:04 +0100

    check sys.argv arguments and clarify code

--- cptools2/__main__.py
+++ cptools2/__main__.py
@@ -1,10 +1,22 @@
 import sys
+import os
 from cptools2 import job
 from cptools2 import parse_yaml
 
 def main():
     """run cptools.job.Job on a yaml file containing arguments"""
-    yaml_dict = parse_yaml.open_yaml(sys.argv[1])
+    # check arguments
+    if len(sys.argv) < 2:
+        msg = "missing argument: need to pass a config file as an argument"
+        raise ValueError(msg)
+    config_file = sys.argv[1]
+    if os.path.isfile(config_file) is False:
+        msg = "'{}' is not a file".format(config_file)
+        raise ValueError(msg)
+    # parse yaml file into a dictionary
+    yaml_dict = parse_yaml.open_yaml(config_file)
+    # check the arguments in the yaml file are recognised
+    parse_yaml.check_yaml_args(yaml_dict)
     # parse all possible commands
     experiment_args = parse_yaml.experiment(yaml_dict)
     chunk_args = parse_yaml.chunk(yaml_dict)
