ce063cd57827cf0565d84901a4629322888d3795
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-10-23 17:26:14 +0100

    point to new parserix location

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name="cptools2",
       license="MIT",
       packages=["cptools2"],
       tests_require=["pytest"],
-      dependency_links=["https://github.com/swarchal/parserix/tarball/master#egg=parserix-0.1"],
+      dependency_links=["https://github.com/carragherlab/parserix/tarball/master#egg=parserix-0.1"],
       install_requires=["pyyaml", "pandas>=0.16", "parserix>=0.1"])
