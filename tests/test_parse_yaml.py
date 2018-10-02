99a11adb1b692170d4b9357d2f4aeb1c9c582faf
Scott Warchal <s.warchal@sms.ed.ac.uk>
2018-10-02 16:46:08 +0100

    update tests for new ConfigParser class

--- tests/test_parse_yaml.py
+++ tests/test_parse_yaml.py
@@ -1,62 +1,63 @@
 import os
+
 import pytest
 from cptools2 import parse_yaml
 
 CURRENT_PATH = os.path.dirname(__file__)
 TEST_PATH = os.path.join(CURRENT_PATH, "test_config.yaml")
 TEST_BROKEN = os.path.join(CURRENT_PATH, "test_config_broken.yaml")
+TEST_NO_CHUNK = os.path.join(CURRENT_PATH, "test_config_no_chunk.yaml")
 
 def test_open_yaml():
-    """cptools2.parse_yaml.open_yaml(path_to_yaml)"""
-    yaml_dict = parse_yaml.open_yaml(TEST_PATH)
-    assert isinstance(yaml_dict, dict)
+    config = parse_yaml.ConfigParser(TEST_PATH)
+    config_contents = config.config_contents
+    assert isinstance(config_contents, dict)
 
 
-def test_check_yaml_args():
-    """cptools2.parse_yaml.check_yaml_args(yaml_dict)"""
-    yaml_dict = parse_yaml.open_yaml(TEST_BROKEN)
+def test_check_config_is_valid():
     with pytest.raises(ValueError):
-        parse_yaml.check_yaml_args(yaml_dict)
+        parse_yaml.ConfigParser(TEST_BROKEN)
 
 
 def test_experiment():
-    """cptools2.parse_yaml.experiment(yaml_dict)"""
-    yaml_dict = parse_yaml.open_yaml(TEST_PATH)
-    output = parse_yaml.experiment(yaml_dict)
-    assert output == {"exp_dir" : "/path/to/experiment"}
+    config = parse_yaml.ConfigParser(TEST_PATH)
+    output = config.experiment
+    assert output == {"exp_dir": "/path/to/experiment"}
 
 
 def test_chunk():
-    """cptools2.parse_yaml.chunk(yaml_dict)"""
-    yaml_dict = parse_yaml.open_yaml(TEST_PATH)
-    output = parse_yaml.chunk(yaml_dict)
-    assert output == {"job_size" : 46}
+    config = parse_yaml.ConfigParser(TEST_PATH)
+    output = config.chunk
+    assert output == {"job_size": 46}
 
 
 def test_add_plate():
-    """cptools2.parse_yaml.add_plate(yaml_dict)"""
-    yaml_dict = parse_yaml.open_yaml(TEST_PATH)
-    output = parse_yaml.add_plate(yaml_dict)
+    config = parse_yaml.ConfigParser(TEST_PATH)
+    output = config.add_plate
     assert output == {"exp_dir" : "/path/to/new/experiment",
                       "plates" : ["plate_3", "plate_4"]}
 
 
 def test_remove_plate():
-    """cptools2.parse_yaml.remove_plate(yaml_dict):"""
-    yaml_dict = parse_yaml.open_yaml(TEST_PATH)
-    output = parse_yaml.remove_plate(yaml_dict)
+    config = parse_yaml.ConfigParser(TEST_PATH)
+    output = config.remove_plate
     assert output == {"plates" : ["plate_1", "plate_2"]}
 
 
-def test_create_commands():
-    """cptools2.parse_yaml.create_commands(yaml_dict)"""
-    yaml_dict = parse_yaml.open_yaml(TEST_PATH)
-    output = parse_yaml.create_commands(yaml_dict)
+def test_pipeline():
+    config = parse_yaml.ConfigParser(TEST_PATH)
+    output = config.pipeline
     pipeline_loc = os.path.abspath("./tests/example_pipeline.cppipe")
-    assert output == {"pipeline" : pipeline_loc,
-                      "location" : "/example/location",
-                      "commands_location" : "/home/user",
-                      "job_size": 46}
+    assert output == {"pipeline": pipeline_loc}
 
 
+def test_location():
+    config = parse_yaml.ConfigParser(TEST_PATH)
+    output = config.location
+    assert output == {"location": "/example/location"}
 
+
+def test_commands_location():
+    config = parse_yaml.ConfigParser(TEST_PATH)
+    output = config.commands_location
+    assert output == {"commands_location": "/home/user"}
